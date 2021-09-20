91c1bef317d5420d63fed95c324826d91b813a0a
wkrzyzanowski <wiktor.krzyzanowski1@gmail.com>
2021-09-20 20:22:19 +0200

    Extended server.go in order to handle middleware. Implemented middleware example.

--- todox-backend/server/api.go
+++ todox-backend/server/api.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const BASE_API_URL = "/api"
+
 type ApiEndpoint struct {
 	RelativePath string
 	HttpMethod   string
@@ -12,4 +14,9 @@ type ApiEndpoint struct {
 
 type ApiController interface {
 	GetEndpoints() []ApiEndpoint
+}
+
+type ApiMiddleware struct {
+	Name     string
+	Function gin.HandlerFunc
 }
